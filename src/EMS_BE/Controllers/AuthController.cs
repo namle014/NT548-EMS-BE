38214c7d606bc607aa18a93cdd640d4df5865c17
kris <ng.phihoc123@gmail.com>
2025-05-24 05:28:01 +0700

    update api test

--- src/EMS_BE/Controllers/AuthController.cs
+++ src/EMS_BE/Controllers/AuthController.cs
@@ -45,6 +45,14 @@ namespace OA.WebAPI.Controllers
             return File(exportStream.Stream, exportStream.ContentType, exportStream.FileName);
         }
 
+        [HttpGet("getRandomNumber")]
+        [AllowAnonymous]
+        public IActionResult GetRandomNumber()
+        {
+            var random = new Random();
+            int number = random.Next(1, 100);
+            return Ok(new { RandomNumber = number });
+        }
 
         [HttpGet]
         [Authorize]
